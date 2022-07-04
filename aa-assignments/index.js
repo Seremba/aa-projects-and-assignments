b7072b5be68944fb4a93f5335c42c3e2ef9901fe
Seremba <pserembae@gmail.com>
2022-07-04 13:16:29 +0300

    Solve Choosey Endings challenge

--- aa-assignments/index.js
+++ aa-assignments/index.js
@@ -350,3 +350,42 @@ let longWords = function(words) {
 
 console.log(longWords(['bike', 'skateboard','scooter', 'moped'])); // [ 'skateboard', 'scooter' ]
 console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // [ 'couscous', 'ceviche', 'solyanka' ]
+
+
+/*
+Write a function chooseyEndings that accepts an array of words and a suffix string as arguments.
+The function should return a new array containing the words that end in the given suffix.
+If the value passed in is not an array, return an empty array. HINT: There are built in JavaScript functions that will help with determining if a strings ends a certain way.
+Go see if you can find it on MDN!
+*/
+
+// Your code here
+let chooseyEndings = function(words, str){
+
+    let sufixArray = []
+
+    for(let i = 0; i < words.length; i++){
+        let word = words[i]
+
+        if(word.endsWith(str)){
+            sufixArray.push(word)
+        }
+    }
+    return sufixArray
+}
+
+
+console.log(chooseyEndings(['family', 'hound', 'catalyst','fly', 'timidly', 'bond'], 'ly'));
+// [ 'family', 'fly', 'timidly' ]
+
+console.log(chooseyEndings(['family', 'hound', 'catalyst','fly', 'timidly', 'bond'], 'nd'));
+// [ 'hound', 'bond' ]
+
+console.log(chooseyEndings(['simplicity', 'computer', 'felicity'], 'icity'));
+// [ 'simplicity', 'felicity' ]
+
+console.log(chooseyEndings(['simplicity', 'computer', 'felicity'], 'ily'));
+// [ ]
+
+console.log(chooseyEndings(17, 'ily'));
+// [ ]
