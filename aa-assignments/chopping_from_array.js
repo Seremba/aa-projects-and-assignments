c9cdea34177ba5c2e70031ecb3f470cea984c02b
Seremba <pserembae@gmail.com>
2022-07-04 19:07:13 +0300

    solve more challenges

--- aa-assignments/chopping_from_array.js
+++ aa-assignments/chopping_from_array.js
@@ -59,3 +59,71 @@ console.log(arr1); // [ 'a', 'b', 'c' ]
 let arr2 = ['kale', 'spinach', 'collard greens', 'cabbage'];
 console.log(popper(arr2, 1)); // [ 'cabbage' ]
 console.log(arr2); // [ 'kale', 'spinach', 'collard greens' ]
+
+
+/*
+Rotate Right
+Write a function rotateRight(array, num) that takes in an array and a number as args.
+The function should return a new array where the elements of the array are rotated to the right
+num times. The function should not mutate the original array and instead return a new array.
+
+Define this function using function expression syntax.
+
+HINT: you can use Array's slice() method to create a copy of an array
+*/
+// your code here
+let rotateRight = function(array, num){
+    let newArray = array.slice()
+
+    for(let i = 0; i < num; i++){
+        let el = array.pop()
+        newArray.unshift(el)
+    }
+
+    return newArray
+}
+
+
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(rotateRight(arr, 2)); // [ 'd', 'e', 'a', 'b', 'c' ]
+console.log(arr); // [ 'a', 'b', 'c', 'd', 'e' ]
+
+let animals = ['wombat', 'koala', 'opossum', 'kangaroo'];
+console.log(rotateRight(animals, 3)); // [ 'koala', 'opossum', 'kangaroo', 'wombat' ]
+console.log(animals); // [ 'wombat', 'koala', 'opossum', 'kangaroo' ]
+
+/*
+Rotate
+Write a function rotate(array, num) that takes in an array and a number as args.
+When the num is positive, the elements of the array should be rotated to the right.
+When the num is negative, the elements of the array should be rotated to the left.
+The function should mutate the original array.
+
+Define this function using function expression syntax.
+*/
+
+// your code here
+let rotate = function(array, num) {
+    if(num > 0){
+        for(let i = 0; i < num ; i++){
+            let el = array.pop()
+            array.unshift(el)
+        }
+    } else if(num < 0){
+        for(let i = 0; i < -(num); i++){
+            let el = array.shift()
+            array.push(el)
+        }
+    }
+}
+
+
+
+ ;
+rotate(['a', 'b', 'c', 'd', 'e'], 2);
+console.log(arr); // [ 'd', 'e', 'a', 'b', 'c' ]
+
+
+rotate(['wombat', 'koala', 'opossum', 'kangaroo'], -1);
+console.log(animals); // [ 'koala', 'opossum', 'kangaroo', 'wombat' ]
