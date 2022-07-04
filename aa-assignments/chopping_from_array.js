0b7d8c2d736a82e956af11bad1e8c2438c16bf0b
Seremba <pserembae@gmail.com>
2022-07-05 01:25:13 +0300

    Solve reverseSentence challenge

--- aa-assignments/chopping_from_array.js
+++ aa-assignments/chopping_from_array.js
@@ -179,3 +179,33 @@ let longestWord = function(sentence){
 console.log(longestWord('where did everyone go')); // 'everyone'
 console.log(longestWord('prefer simplicity over complexity')); // 'simplicity'
 console.log(longestWord('')); // ''
+
+/*
+Reverse Sentence
+Write a function reverseSentence(sentence) that takes in a sentence as an arg.
+The function should return a new sentence where the order of the words is reversed.
+Note that you should reverse the order among words,
+not the order among characters.
+*/
+
+// your code here
+let reverseSentence = function(sentence){
+
+    let words = sentence.split(' ')
+    let reversed = []
+
+    for(let i = words.length - 1; i >= 0; i--){
+        let word = words[i]
+        // console.log( word)
+        reversed.push(word)
+    }
+
+    return reversed.join(' ')
+}
+
+
+
+
+
+console.log(reverseSentence('I am pretty hungry')); // 'hungry pretty am I'
+console.log(reverseSentence('follow the yellow brick road')); // 'road brick yellow the follow'
