a80ea2d22ebe97a454e3439b5c1de679cf43432f
Seremba <pserembae@gmail.com>
2022-07-06 12:54:39 +0300

    Solve twoSum challenge

--- aa-assignments/practice.js
+++ aa-assignments/practice.js
@@ -127,3 +127,39 @@ console.log(myIndexOf(['a', 'b', 'c', 'e'], 'e')); // 3
 console.log(myIndexOf(['a', 'b', 'c', 'e'], 'z')); // -1
 console.log(myIndexOf([43, -7, 11, 13, 43], 43)); // 0
 console.log(myIndexOf([43, -7, 11, 13], 1)); // -1
+
+
+/*
+Two Sum Recall
+Write a function twoSum(arr, target) that accepts an array and a target number as args.
+The function should a return a boolean indicating if two distinct numbers of the array add up to the target value.
+You can assume that input array contains only unique numbers.
+*/
+
+// your code here
+let twoSum = function(arr, target){
+
+    for(let i = 0; i < arr.length; i++){
+        let el1 = arr[i]
+
+        for(let j = i + 1; j < arr.length; j++){
+            let el2 = arr[j]
+
+            let sum = el1 + el2
+
+            if(sum === target){
+                return true
+            }
+        }
+    }
+
+    return false
+}
+
+
+
+
+console.log(twoSum([1, 7, 3, 0, 2], 5)); // true
+console.log(twoSum([1, 7, 3, 0, 2], 6)); // false
+console.log(twoSum([4, 6, 2, 3], 8)); // true
+console.log(twoSum([4, 6, 2, 3], 11)); // false
