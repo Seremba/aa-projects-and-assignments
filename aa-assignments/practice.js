7d807043423c0351560dd866e4fe9336a1995c31
Seremba <pserembae@gmail.com>
2022-07-06 12:37:51 +0300

    Solve My Index and Pig Latin challenges

--- aa-assignments/practice.js
+++ aa-assignments/practice.js
@@ -60,3 +60,70 @@ function sumArray(array) {
 
 console.log(sumArray([5, 6, 4])); // => 15
 console.log(sumArray([7, 3, 9, 11])); // => 30
+
+/*
+Pig Latin Recall
+Pig Latin is a fun take on the English language where you move any consonant cluster from the start of the word to the
+end of the word; when words begin on a vowel, you simply add "-yay". Vowels are "aeiou".
+
+Write a function pigLatinWord that takes in a word string and translates the word into Pig Latin.
+For this problem use the String.slice() method. The slice() method extracts a section of a string and
+returns it as a new string, without modifying the original string.
+
+Hint: Remember the String.includes method!
+*/
+
+// So the two rules for our version of Pig Latin are:
+
+// 1. For words that start with a vowel, add 'yay' to the end of the word.
+// 2. For words that start with a non-vowel, move all letters that come
+// **before the first vowel** to the **end of the word** then add 'ay'
+
+function pigLatinWord(word) {
+    // your code here...
+    let vowels = 'aeiou';
+
+    if(vowels.includes(word[0])){
+        return word + "yay";
+    }
+
+    for(let i = 0; i < word.length; i++){
+        let letter = word[i]
+
+        if(vowels.includes(letter)){
+           let firstPart = word.slice(i)
+           let  secondPart = word.slice(0, i)
+
+           return firstPart + secondPart + "ay"
+        }
+    }
+  };
+
+  console.log(pigLatinWord("apple")); //=> "appleyay"
+  console.log(pigLatinWord("eat")); //=> "eatyay"
+  console.log(pigLatinWord("banana")); //=> "ananabay"
+  console.log(pigLatinWord("trash")); //=> "ashtray"
+
+  /*
+  My Index Of Recall
+Write a function myIndexOf(arr, target) that takes in an array and target value as args.
+The function should return the first index where the target is found in the array.
+If the target is not found, it should return -1. Solve this without using Array.indexOf.
+  */
+// your code here
+let myIndexOf = function(arr, target){
+
+    for( let i = 0; i < arr.length; i++){
+        if(arr[i] === target){
+            return i
+        }
+    }
+
+    return -1
+}
+
+console.log(myIndexOf(['a', 'b', 'c', 'e'], 'c')); // 2
+console.log(myIndexOf(['a', 'b', 'c', 'e'], 'e')); // 3
+console.log(myIndexOf(['a', 'b', 'c', 'e'], 'z')); // -1
+console.log(myIndexOf([43, -7, 11, 13, 43], 43)); // 0
+console.log(myIndexOf([43, -7, 11, 13], 1)); // -1
