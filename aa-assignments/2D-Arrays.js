dd2d80d82e31046414cdfc0843dd0edb66d2c832
Seremba <pserembae@gmail.com>
2022-07-06 09:34:52 +0300

    Solve addPyramid challenge

--- aa-assignments/2D-Arrays.js
+++ aa-assignments/2D-Arrays.js
@@ -239,3 +239,67 @@ console.log(matrixAddition(matrixA, matrixB)); // [[11, 6], [7, 7]]
 console.log(matrixAddition(matrixA, matrixC)); // [[1, 5], [4, 6]]
 console.log(matrixAddition(matrixB, matrixC)); // [[8, 1], [3, -1]]
 console.log(matrixAddition(matrixD, matrixE)); // [[2, -5], [19, 14], [6, 4]]
+
+
+/*
+Pyramid Array
+Write a function pyramidArray(base) that takes in an array of numbers representing the base of a pyramid.
+The function should return a two-dimensional array representing the completed pyramid.
+To generate an element of the next level of the pyramid, we sum the elements below and to the left and below
+and to the right.
+*/
+
+// For example, given 2, 3, 7, 5, 9 as the base, we should construct this pyramid:
+//
+//           85
+//         37  48
+//       15  22  26
+//    5   10   12   14
+//  2   3    7    5    9
+//
+//
+// Your code here
+
+let pyramidArray = function(base){
+
+    let pyramid = [base];
+
+    while(pyramid.length < base.length){
+
+        let next = sumOfElementsBelow(pyramid[0])
+
+        pyramid.unshift(next)
+    }
+
+    return pyramid
+}
+
+let sumOfElementsBelow = function(array){
+    let sums = []
+
+    for(let i = 0; i < array.length - 1; i++){
+        sum = array[i] + array[i + 1]
+        sums.push(sum)
+    }
+
+    return sums
+}
+
+let p1 = pyramidArray([2, 3, 7, 5, 9]);
+console.log(p1);
+// [
+//   [ 85 ],
+//   [ 37, 48 ],
+//   [ 15, 22, 26 ],
+//   [ 5, 10, 12, 14 ],
+//   [ 2, 3, 7, 5, 9 ]
+// ]
+
+let p2 = pyramidArray([2, 2, 2, 2]);
+console.log(p2);
+// [
+//   [ 16 ],
+//   [ 8, 8 ],
+//   [ 4, 4, 4 ],
+//   [ 2, 2, 2, 2 ]
+// ]
