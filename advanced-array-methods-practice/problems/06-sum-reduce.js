d52ae325b6f9aa476e12ef3ada5f1d308fdaa145
Seremba <pserembae@gmail.com>
2022-07-07 15:09:08 +0300

    Solve sum reduce challenge

--- advanced-array-methods-practice/problems/06-sum-reduce.js
+++ advanced-array-methods-practice/problems/06-sum-reduce.js
@@ -14,8 +14,13 @@ console.log(sumWithReduce([])); // 0
 
 let sumWithReduce = function(nums) {
   // Your code here
+  let sum = nums.reduce(function(acc, el) {
+      return acc + el
+    })
+    return sum
 };
 
+console.log(sumWithReduce([10, 3, 5, 2])); // 20
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
