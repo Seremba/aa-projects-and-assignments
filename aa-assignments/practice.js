790cb507419d2fd5131d4a34283ef85a83b83202
Seremba <pserembae@gmail.com>
2022-07-07 13:14:32 +0300

    Solve first forEach problem

--- aa-assignments/practice.js
+++ aa-assignments/practice.js
@@ -163,3 +163,83 @@ console.log(twoSum([1, 7, 3, 0, 2], 5)); // true
 console.log(twoSum([1, 7, 3, 0, 2], 6)); // false
 console.log(twoSum([4, 6, 2, 3], 8)); // true
 console.log(twoSum([4, 6, 2, 3], 11)); // false
+
+/*
+Remove Last Vowel Recall
+Write a function removeLastVowel(word) that
+takes in a string and returns the string with its last vowel removed.
+*/
+
+
+let removeLastVowel = function(word){
+    let vowels = 'aeiou'
+    for(let i = word.length - 1; i >= 0; i--){
+        let char = word[i]
+
+        if(vowels.includes(char)){
+            return word.slice(0, i) + word.slice(i + 1)
+        }
+    }
+}
+
+
+console.log(removeLastVowel('bootcamp')); // 'bootcmp'
+console.log(removeLastVowel('better')); // 'bettr'
+console.log(removeLastVowel('graph')); // 'grph'
+console.log(removeLastVowel('thy')); // 'thy'
+
+
+/*
+Least Common Multiple Recall
+Write a function leastCommonMultiple(num1, num2) that accepts two numbers as arguments.
+The functions should return the smallest number that is divisible by both num1 and num2.
+*/
+// your code here
+let leastCommonMultiple = function(num1, num2){
+    for(let i = 1; i <= (num1 * num2); i++){
+
+        if(i % num1 === 0 && i % num2 === 0){
+            return i
+        }
+    }
+}
+
+
+console.log(leastCommonMultiple(4, 6)); // 12
+console.log(leastCommonMultiple(3, 5)); // 15
+console.log(leastCommonMultiple(2, 10)); // 10
+
+/*
+Uncompress Recall
+Write a function uncompress(str) that takes in a "compressed" string as an arg.
+A compressed string consists of a character immediately followed by the number of times
+it appears in the "uncompressed" form. The function should return the uncompressed version of the string.
+See the examples.
+
+Hint: you can use the built-in Number function should convert a numeric string into the number type. For example. Number("4") // => 4
+
+You may assume that the number of times will always be an integer between 0 and 9
+*/
+
+// your code here
+let uncompress = function(str){
+    let string = ''
+
+    for(let i = 0; i < str.length; i += 2){
+        let element = str[i]
+        let num = Number(str[i + 1])
+
+        for(let j = 0; j < num; j++){
+            string += element
+        }
+    }
+
+    return string
+}
+
+
+
+
+console.log(uncompress('x3y4z2')); // 'xxxyyyyzz'
+// console.log(uncompress('a5b2c4z1')); // 'aaaaabbccccz'
+// console.log(uncompress('b1o2t1')); // 'boot'
