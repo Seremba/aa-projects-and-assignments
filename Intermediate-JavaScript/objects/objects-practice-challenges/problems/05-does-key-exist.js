018c31bf82637ac08508aa9b07c62c56253d485a
Seremba <pserembae@gmail.com>
2022-07-12 13:52:52 +0300

    Solve does-key-exist challenge

--- Intermediate-JavaScript/objects/objects-practice-challenges/problems/05-does-key-exist.js
+++ Intermediate-JavaScript/objects/objects-practice-challenges/problems/05-does-key-exist.js
@@ -12,7 +12,15 @@ doesKeyExist(obj1, 'name'); // => false
 
 function doesKeyExist(obj, key) {
   // your code here
+  if(obj[key] !== undefined){
+    return true;
+  } else {
+    return false;
+  }
 }
 
+let obj1 = {bootcamp: 'App Academy', course: 'Bootcamp Prep'}
+console.log(doesKeyExist(obj1, 'course')); // => true
+console.log(doesKeyExist(obj1, 'name')); // => false
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = doesKeyExist;
