820886cec983bd2c0ac07e193b92ba4f08f4387c
Seremba <pserembae@gmail.com>
2022-07-15 01:18:01 +0300

    solve arrow-full-name and arrow-my-map challenges

--- Intermediate-JavaScript/scopes-and-closures/scope-problems-without-context-starter/problems/01-arrow-full-name.js
+++ Intermediate-JavaScript/scopes-and-closures/scope-problems-without-context-starter/problems/01-arrow-full-name.js
@@ -1,6 +1,6 @@
 /***********************************************************************
 Write a function using fat arrow syntax, `arrowGetFullName(person)` that takes in
-a person object and returns a string containing their full name. 
+a person object and returns a string containing their full name.
 
 Assign the below function to a variable using the const keyword. Using the const
 keyword will allow any value assigned to that variable protection from being
@@ -13,7 +13,20 @@ let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
 arrowGetFullName(p2); // => 'Charlie Brown'
 
 ***********************************************************************/
+const arrowGetFullName = obj => {
+  let namesArray = [];
+  let firstName = obj.firstName;
+  let lastName  = obj.lastName;
+  namesArray.push(firstName, lastName);
 
+  return namesArray.join(' ');
+}
+
+let p1 = {firstName: 'John', lastName: 'Doe'};
+console.log(arrowGetFullName(p1)); // => 'John Doe'
+
+let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
+console.log(arrowGetFullName(p2)); // => 'Charlie Brown'
 // your code here!
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
