3312df146b156ce0e295746d008abf020adfd8ab
Seremba <pserembae@gmail.com>
2022-07-15 13:35:41 +0300

    solve arrow-mirror-array and closure-sandwiches challenges

--- Intermediate-JavaScript/scopes-and-closures/scope-problems-without-context-starter/problems/05-arrow-mirror-array.js
+++ Intermediate-JavaScript/scopes-and-closures/scope-problems-without-context-starter/problems/05-arrow-mirror-array.js
@@ -12,6 +12,21 @@ arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b
 
 // your code here
 
+ const arrowMirrorArray = array => {
+   let newArray = []
+   array.forEach((el) => {
+     return newArray.push(el)
+   })
+
+   for(let i = array.length - 1; i >=0; i--){
+     let el = array[i];
+     newArray.push(el);
+   }
+
+
+  return newArray
+ }
+ console.log(arrowMirrorArray([1,2,3])); // => [ 1, 2, 3, 3, 2, 1 ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
