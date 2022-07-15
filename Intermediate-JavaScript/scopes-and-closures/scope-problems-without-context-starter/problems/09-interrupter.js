529f0c5c38c447a6c797216de9a5bfdf002b5708
Seremba <pserembae@gmail.com>
2022-07-15 15:38:36 +0300

    Solve interrupter challenge

--- Intermediate-JavaScript/scopes-and-closures/scope-problems-without-context-starter/problems/09-interrupter.js
+++ Intermediate-JavaScript/scopes-and-closures/scope-problems-without-context-starter/problems/09-interrupter.js
@@ -9,7 +9,7 @@ console.log(rudePerson("how are you")); // prints "how what are what you"
 console.log(rudePerson("I like pie")); // prints "I what like what pie"
 
 
-Invoking the interrupter function again: 
+Invoking the interrupter function again:
 let rudePerson2 = interrupter("yo"); // => returns a function
 console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
@@ -17,7 +17,25 @@ console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 ***********************************************************************/
 
 // your code here!
+const interrupter = interruptingWord => {
 
+  return (sentence) => {
+     let words = sentence.split(" ");
+     let newWord = ""
+     for(let i = 0; i < words.length; i++){
+       let word = words[i];
+       if( i === words.length - 1){
+         newWord += word;
+       } else {
+          newWord += word + " " + interruptingWord + " ";
+       }
+     }
+     return newWord
+  }
+}
+
+let rudePerson = interrupter("what"); // => returns a function
+console.log(rudePerson("how are you")); // prints "how what are what you"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
